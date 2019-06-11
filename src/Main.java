fbf65354df52979a9bff8071b2905e98e43ea290
mike1018 <mixasikkaliberdiev3@yandex.ru>
2019-06-11 20:31:48 +0400

    task2(not yet)

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,87 @@
 import static java.lang.System.*;
+import static java.lang.Math.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
-    static int lowest;
     public static void main(String[] args) {
 	// write your code here
-        int arr4 [] = {6,5,4,3};
+        Scanner scan = new Scanner(in);
 
-        out.printf("Array has a lowest: %d\n", lowest_of_arr2(arr4));
-        out.printf("Array has a lowest: %d\n", lowest_of_arr3(arr4));
-        out.printf("Array has a lowest: %d\n", lowest_of_arr4(arr4));
+        out.println("Input array size");
+        int arr_size = scan.nextInt();
+        out.println("Input searched number");
+        int searched = scan.nextInt();
 
+        int array[] = new int[arr_size];
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = (int)(random()*10);
+        }
+        print_arr(array);
+
+        // one by one
+        boolean searched_in = one_by_one_search(searched, array);
+        if(searched_in)
+            out.println("true");
+        else{
+            out.println("false");
+        }
+
+        //binary
+        Arrays.sort(array);
+        print_arr(array);
+        searched_in = false;
+        searched_in = binary_search(searched, array);
+        if(searched_in)
+            out.println("true");
+        else{
+            out.println("false");
+        }
     }
-    static int lowest_of_arr2(int... arr){
-        return lowest = (arr[0] < arr[1]) ? arr[0] : arr[1];
+
+    static void print_arr(int... array){
+        for(int a : array){
+            out.print(a + " ");
+        }
     }
-    static int lowest_of_arr3(int... arr){
-        int of_2 = lowest_of_arr2(arr);
-        return lowest = (of_2 < arr[2]) ? of_2 : arr[2];
+    static boolean one_by_one_search(int searched,int... array){
+        boolean in = false;
+        for(int a : array){
+            if(a == searched){
+                in = true;
+                break;
+            }
+        }
+        return in;
     }
-    static int lowest_of_arr4(int... arr){
-        int of_3 = lowest_of_arr3(arr);
-        return lowest = (of_3 < arr[3]) ? of_3 : arr[3];
+/////////////////////////////////////////////////////////////////////////////
+    static boolean binary_search(int searched,int... array){
+        boolean in = false;
+        int min_index = 0, max_index = array.length - 1;
+        int compared_with = (min_index+max_index)/2;
+
+        do {
+            if (array[compared_with] < searched) {
+
+                min_index = compared_with;
+                compared_with = (min_index+max_index)/2;
+            }
+            else{
+                if (array[compared_with] > searched) {
+                    max_index = compared_with;
+                    compared_with = (min_index+max_index)/2;
+                }
+                else{
+                    return in;
+                }
+
+            }
+        }
+        while((max_index - min_index) >= 0);
+        return in;
     }
+/////////////////////////////////////////////////////////////////////////////
+
 }
-
-
---
+++ src/one.java
@@ -0,0 +1,31 @@
+/*
+import static java.lang.System.*;
+
+
+public class Main {
+
+    static int lowest;
+    public static void main(String[] args) {
+        // write your code here
+        int arr4 [] = {6,5,4,3};
+
+        out.printf("Array has a lowest: %d\n", lowest_of_arr2(arr4));
+        out.printf("Array has a lowest: %d\n", lowest_of_arr3(arr4));
+        out.printf("Array has a lowest: %d\n", lowest_of_arr4(arr4));
+
+    }
+    static int lowest_of_arr2(int... arr){
+        return lowest = (arr[0] < arr[1]) ? arr[0] : arr[1];
+    }
+    static int lowest_of_arr3(int... arr){
+        int of_2 = lowest_of_arr2(arr);
+        return lowest = (of_2 < arr[2]) ? of_2 : arr[2];
+    }
+    static int lowest_of_arr4(int... arr){
+        int of_3 = lowest_of_arr3(arr);
+        return lowest = (of_3 < arr[3]) ? of_3 : arr[3];
+    }
+}
+*/
+
+
