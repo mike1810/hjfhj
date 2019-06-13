d85892c4d761831cd843bb257f2a9863720715aa
mike1018 <mixasikkaliberdiev3@yandex.ru>
2019-06-13 18:46:52 +0400

    Java Fundamentals Practice #2: Search for an element in the array

--- src/task2/task2.java
+++ src/task2/task2.java
@@ -1,4 +1,5 @@
 package task2;
+
 import static java.lang.System.*;
 import static java.lang.Math.*;
 import java.util.Arrays;
@@ -11,26 +12,45 @@ public class task2 {
         // write your code here
         Scanner scan = new Scanner(in);
 
+        int arr_size;
+        boolean check = true;
+        do {
+            out.println((check)?"Input array size(above 0):":("Input array size again, last value is not above 0:"));
+            arr_size = scan.nextInt();
+            check = false;
+        }
+        while(arr_size<=0);
 
-        out.println("If u need to see, how binary search better then one-by-ine, u can input array size = 1000000 and searched number = 1000001");
-
-        out.println("Input array size");
-        int arr_size = scan.nextInt();
-        out.println("Input searched number");
-        int searched = scan.nextInt();
+        check = true; int N = 1000000;
+        int searched;
+        do {
+            out.println((check)?("Input searched number (which in [0;" + N + "]):"):("Input searched number  again, last value is not correct"));
+            searched = scan.nextInt();
+        }
+        while(searched > N || searched < 0);
 
         int array[] = new int[arr_size];
 
         for(int i = 0; i < array.length; i++){
-            array[i] = (int)(random()*1000);
+            array[i] = (int)(random()*N);
         }
-        //if u need to see array numbers, uncomment next line
-        //print_arr(array);
+
+        out.println("if u need to see arrays later, press '1', or something else if not ");
+
+        boolean need_to_see;// need to call print_arr();
+        String s_need_to_see = scan.next();
+        switch(s_need_to_see){
+            case "1": need_to_see = true; break;
+            default: need_to_see = false; break;
+        }
 
         long startTime, finishTime, executionTime;// need to check execution
 
         // one by one
-        out.println("One by one search started");
+        out.println("\nOne by one search started");
+        if(need_to_see){
+            print_arr(array);
+        }
         startTime = System.nanoTime();
         boolean searched_in = one_by_one_search(searched, array);//one by one search
         finishTime = System.nanoTime();
@@ -39,10 +59,11 @@ public class task2 {
         out.println("One by one search finished in " + executionTime + " nanoseconds");
 
         //binary
-         out.println("Binary search start");
         Arrays.sort(array);//array sorting
-        //if u need to see array numbers, uncomment next line
-        //print_arr(array);
+        if(need_to_see) {
+            print_arr(array);
+        }
+        out.println("\nBinary search started");
         startTime = System.nanoTime();
         searched_in = binary_search(searched, array);
         finishTime = System.nanoTime();
@@ -52,15 +73,12 @@ public class task2 {
     }
 
     static void print_result( boolean searched_in, int searched){
-
-        if(searched_in)
-            out.println("Number " + searched +  " is in array");
-        else{
-            out.println("Number " + searched +  " is not in array");
-        }
+        out.println((searched_in)?("Number " + searched +  " is in array"):("Number " + searched +  " is not in array"));
     }
 
     static void print_arr(int... array){
+
+        out.println("\nYour array:");
         for(int a : array){
             out.print(a + " ");
         }
@@ -76,30 +94,35 @@ public class task2 {
         }
         return in;
     }
-    /////////////////////////////////////////////////////////////////////////////
     static boolean binary_search(int key,int... array){
         boolean in = false;
         int min_index = 0, max_index = array.length - 1;
         int compared_with;
 
         while(max_index > min_index){
-            compared_with = (min_index+max_index)/2;
-            if (array[compared_with] < key) {
 
-                min_index = compared_with + 1;
-            }
-            else{
-                if (array[compared_with] > key) {
-                    max_index = compared_with - 1;
-                    compared_with = (min_index+max_index)/2;
-                }
-                else{
+            compared_with = (min_index+max_index)/2;
+            if(max_index - min_index == 2){
+                if(array[min_index+1] == key)
+                {
                     in = true;
                     return in;
+                }
+            }
+
+            if(array[compared_with] == key){
+                in = true;
+                return in;
+            }
+            else{
+                if(array[compared_with] < key){
+                    min_index = compared_with + 1;
+                }
+                else{
+                    max_index = compared_with - 1;
                 }
             }
         }
         return in;
     }
-/////////////////////////////////////////////////////////////////////////////
 }
