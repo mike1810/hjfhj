f38cc9a826cd6d681f0686c258ecc1f990729dc3
mike1018 <mixasikkaliberdiev3@yandex.ru>
2019-06-13 18:46:39 +0400

    Java Fundamentals Practice #1: Find the minimum

--- src/task1/task1.java
+++ src/task1/task1.java
@@ -9,20 +9,20 @@ public class task1 {
         // write your code here
         int arr4 [] = {6,5,4,3};
 
-        out.printf("Array has a lowest: %d\n", lowest_of_arr2(arr4));
-        out.printf("Array has a lowest: %d\n", lowest_of_arr3(arr4));
-        out.printf("Array has a lowest: %d\n", lowest_of_arr4(arr4));
+        out.printf("Array has a lowest: %d\n", lowest_of_arr(arr4[0], arr4[1]));
+        out.printf("Array has a lowest: %d\n", lowest_of_arr(arr4[0], arr4[1], arr4[2]));
+        out.printf("Array has a lowest: %d\n", lowest_of_arr(arr4[0], arr4[1], arr4[2], arr4[3]));
+    }
 
+    static int lowest_of_arr(int x, int y){
+        return lowest = (x < y) ? x : y;
     }
-    static int lowest_of_arr2(int... arr){
-        return lowest = (arr[0] < arr[1]) ? arr[0] : arr[1];
+    static int lowest_of_arr(int x, int y, int z){
+        int of_2 = lowest_of_arr(x,y);
+        return lowest = (of_2 < z) ? of_2 : z;
     }
-    static int lowest_of_arr3(int... arr){
-        int of_2 = lowest_of_arr2(arr);
-        return lowest = (of_2 < arr[2]) ? of_2 : arr[2];
-    }
-    static int lowest_of_arr4(int... arr){
-        int of_3 = lowest_of_arr3(arr);
-        return lowest = (of_3 < arr[3]) ? of_3 : arr[3];
+    static int lowest_of_arr(int x, int y, int z, int u){
+        int of_3 = lowest_of_arr(x,y,z);
+        return lowest = (of_3 < u) ? of_3 : u;
     }
 }
