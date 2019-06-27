2cf4e2d0e0730b3b89a1089f3c4e17737c4634a9
mike1810 <51641672+mike1810@users.noreply.github.com>
2019-06-27 23:40:24 +0400

    Collections, tasks near to be ready, but it`s not a last version. It will be ready next morning

--- src/Collections/CustomLinkedList/CustomLinkedList.java
+++ src/Collections/CustomLinkedList/CustomLinkedList.java
@@ -1,49 +1,146 @@
 package Collections.CustomLinkedList;
 
+import com.sun.org.apache.bcel.internal.classfile.DescendingVisitor;
+
+import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
 
-public class CustomLinkedList <T>{
+public class CustomLinkedList <T> implements Iterable<T> {
     private Node <T> head;
     private Node <T> tail;
     private int size;
 
     CustomLinkedList() {
-        this.head = null;
-        this.tail = null;
+        this.head = new Node<>(null, null, tail);
+        this.tail = new Node<>(head, null, null);
         size = 0;
     }
 
+    public void add(T added){
+        addFirst(added);
+    }
 
-    public int length(){
+    public void assIfFirstCollectionElement(T added){
+        Node<T> add = new Node<>(head, added, tail);
+        tail.prev = add;
+        head.next = add;
+        size++;
+    }
+
+    public void addLast(T added){
+        if(size == 0){
+            assIfFirstCollectionElement(added);
+        }else {
+            Node<T> previous = tail;
+            previous.container = added;
+            tail = new Node<>(previous, null, null);
+            previous.next = tail;
+            size++;
+        }
+    }
+
+    public void addFirst(T added){
+        if(size == 0){
+            assIfFirstCollectionElement(added);
+        }else{
+            Node<T> next = head;
+            next.container = added;
+            head = new Node<>(null,null, next);
+            next.prev = head;
+            size++;
+        }
+    }
+
+    public void remove(int index){
+        Node<T> curr = head;
+        for(int i = 0; i < index; i++){
+            curr = curr.next;
+        }
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
+        size--;
+    }
+
+    public void remove(T container){
+        Node<T> curr = head;
+        for(int i = 0; i < size; i++){
+            curr = curr.next;
+            if(curr.container.equals(container)){
+                curr.prev.next = curr.next;
+                curr.next.prev = curr.prev;
+                size--;
+                break;
+            }
+        }
+    }
+
+    public void removeAllElementsWithThisValue(T container){
+        Node<T> curr = head;
+        for(int i = 0; i < size; i++){
+            curr = curr.next;
+            if(curr.container.equals(container)){
+                curr.prev.next = curr.next;
+                curr.next.prev = curr.prev;
+                size--;
+            }
+        }
+    }
+
+    public void removeFirst(T container){
+        if(size > 0){
+            head.next = head.next.next;
+            head.next.next.prev = head;
+        }else{
+            System.out.println("No values to remove");
+        }
+    }
+
+    public void removeLast(T container){
+        if(size > 0){
+            tail.prev = tail.prev.prev;
+            tail.prev.prev.next = tail;
+        }else{
+            System.out.println("No values to remove");
+        }
+    }
+
+    public CustomLinkedList<T> reverseElements(CustomLinkedList<T> current){
+        CustomLinkedList<T> reversed = new CustomLinkedList<>();
+        current.forEach(a->reversed.addFirst(a));
+        current.forEach(a->current.removeAllElementsWithThisValue(a));
+        return reversed;
+    }
+
+    public int size(){
         return size;
     }
 
-    public void add(T added) {
-        if(size == 0){
-            Node<T> addFirst = new Node<>(null, added, null);
-            head = addFirst;
-            size++;
-        }
-        if(size == 1){
-            Node<T> addToList = new Node<>(head, added , null);
-            tail = addToList;
-            head.next = tail;
-            size++;
-        }
-        if(size > 1){
-            Node<T> addToList = new Node<>(tail, added , null);
-            tail.next = addToList;
-            size++;
-        }
+    @Override
+    public Iterator<T> iterator(){
+        return new Iterator<T>() {
+            int counter = 0;
+            @Override
+            public boolean hasNext() {
+                return counter < size;
+            }
+
+            @Override
+            public T next() {
+                return getIndexElement(counter++);
+            }
+        };
     }
 
-
-    public void remove(int index){
-
+    public T getIndexElement(int count){
+        Node<T> needToFind = getNext(head);
+        for(int i = 0; i < count; i++){
+            needToFind = getNext(needToFind);
+        }
+        return needToFind.container;
     }
 
-
-
-
-
+    private Node<T> getNext(Node<T> curr){
+        return curr.next;
+    }
 }
--- src/Collections/CustomLinkedList/Demo.java
+++ src/Collections/CustomLinkedList/Demo.java
@@ -1,5 +1,38 @@
 package Collections.CustomLinkedList;
 
-public class Demo {
+import java.util.Iterator;
+import java.util.LinkedList;
 
+public class Demo {
+        public static void main(String[] args) {
+            CustomLinkedList<Double> arr = new CustomLinkedList<>();
+            arr.addLast(5.5);
+            arr.addLast(4.5);
+            arr.addLast(2.5);
+            arr.addLast(2.5);
+            arr.addLast(4.5);
+            arr.addLast(5.5);
+            arr.addLast(4.5);
+            System.out.println(arr.size());
+
+            arr.remove(2);
+            System.out.println(arr.size());
+
+
+            Iterator iterator = arr.iterator();
+            while(iterator.hasNext()){
+                System.out.println(iterator.next());
+            }
+            System.out.println("___________________________________");
+
+            arr.removeAllElementsWithThisValue(2.5);
+            System.out.println(arr.size());
+
+            System.out.println("__________________________+______");
+            arr.forEach(a -> System.out.println(a));
+            arr = arr.reverseElements(arr);
+            System.out.println("__________________________+______");
+            arr.forEach(a -> System.out.println(a));
+
+        }
 }
