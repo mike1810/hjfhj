8f3c143584ecdd5abb1b22f9dbc6d0030e453e92
mike1018 <mixasikkaliberdiev3@yandex.ru>
2019-07-02 18:29:04 +0400

    LambdaSecondReadyIThink

--- src/obj_bas_pr1/Book.java
+++ src/obj_bas_pr1/Book.java
@@ -1,27 +1,59 @@
 package obj_bas_pr1;
 
+import java.util.*;
+
 import static java.lang.System.out;
 
 public class Book {
 
-    private String title ;
-    private Author author ;
-    private double price ;
-    
-    Book(String title, Author author, double price ){
+    private String title;
+    private Author author;
+    private double price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Book(String title, Author author, double price) {
         this.title = title;
         this.author = author;
         this.price = price;
     }
-    Book(String title, double price ){
+
+    Book(String title, double price) {
         this(title, new Author(), price);
     }
-    protected void bookDetails(){
-        out.println("\nBook title: " + title + "\nBook author: " +
+
+    protected void bookDetails() {
+        out.println("\nBook title:  " + title + "\nBook author: " +
                 author.authorDetailsToBook() + "\nBook price : " + price + "$");
-        //Если отталкиваться от того, что в задании нужно реализовать метод вывода данных об авторе на экран
-        //и этот метод нигде не используется, то можно сделать так:
-        // out.print("\nBook title: " + title + "\nBook author: ");author.printAuthor();
-        //       out.println("Book price : " + price + "$");
     }
+
+    @Override
+    public String toString() {
+        return "\nBook title:  " + title + "\nBook author: " +
+                author.authorDetailsToBook() + "\nBook price : " + price + "$";
+    }
+
+
 }
